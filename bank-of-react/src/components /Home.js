35ccf067c88928f9d315e062fefe84e2b15d39b8
Joy Biswas <joy.biswas77@myhunter.cuny.edu>
2021-11-22 21:46:43 -0500

    updated files

--- bank-of-react/src/components /Home.js
+++ bank-of-react/src/components /Home.js
@@ -1,16 +1,22 @@
-    // src/components/Home.js
-    
-    import React, { Component } from 'react';
-    
-    class Home extends Component {
-      render() {
-        return (
-            <div>
-              <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-              <h1>Bank of React</h1>
-            </div>
-        );
-      }
-    }
-    
-    export default Home;
+// src/components/Home.js
+
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <img src="https://picsum.photos/201" alt="bank"/>
+          <h1>Bank of React</h1>
+          <Link to="/login">Login</Link>
+          <Link to="/userprofile">User Profile</Link>
+          <Link to="/debits">Debits</Link>
+          <Link to="/credits">Credits</Link>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
+    );
+  }
+}
+
+export default Home;
